378beb11c672c949620b99d6599adb80c88be5a4
Cristian Gutiérrez <cristiangutierreztkd@gmail.com>
2021-09-11 21:37:36 -0400

    Se crea proyecto, realizando repositorio en github y se carga angular/material

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,6 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
+import {MatInputModule} from '@angular/material/input';
 
 
 @NgModule({
@@ -12,7 +13,8 @@ import { HomePageRoutingModule } from './home-routing.module';
     CommonModule,
     FormsModule,
     IonicModule,
-    HomePageRoutingModule
+    HomePageRoutingModule,
+    MatInputModule
   ],
   declarations: [HomePage]
 })
